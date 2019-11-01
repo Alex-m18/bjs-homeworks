9be3143bab3ade002a50e42f757640e1002b15c6
Aleksandr Melnikov <melnikov@tikhvin.org>
2019-11-01 21:27:21 +0300

    Added task 2, task 3

--- variables/main.js
+++ variables/main.js
@@ -1,5 +1,15 @@
+let myName = 'Александр';
+let message = (`Привет, мир! Меня зовут ${myName}`);
+console.log(message);
+
 let algebra = 5;
 let geography = 4;
 let physics = 9;
 let average = (algebra + geography + physics) / 3;
 console.log(average);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
