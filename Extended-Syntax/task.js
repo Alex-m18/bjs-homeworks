30a490b1c40201adec8cc5de6ba87b32806d5432
Aleksandr Melnikov <melnikov@tikhvin.org>
2019-11-09 01:19:47 +0300

    Fix task 1, task 2

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,13 +1,13 @@
 "use strict";
 
 function calculateQuadraticEquation(){
-    let a = +window.a.value;
-    let b = +window.b.value;
-    let c = +window.c.value;
-    let result = getResult(a,b,c);
-    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
-    let span = window.result;
-    span.textContent = "х = "+result;
+  let a = +window.a.value;
+  let b = +window.b.value;
+  let c = +window.c.value;
+  let result = getResult(a,b,c);
+  window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+  let span = window.result;
+  span.textContent = "х = "+result;
 }
 
 function getResult(a,b,c){
@@ -18,69 +18,61 @@ function getResult(a,b,c){
   let d = Math.pow(b, 2) - 4 * a * c;
   console.log(d);
       
-  if (d == 0) {
+  if (d === 0) {
     x.push(-b / (2 * a));
   }
   else if (d > 0) {
     x.push((-b - Math.sqrt(d)) / (2 * a));
     x.push((-b + Math.sqrt(d)) / (2 * a));
   }
-  return x; 
-}  
+
+  return x;
+
+} 
+
 getResult();
-//-------------------------------------------------------
 
 function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
-    let averageMark = getAverageMark(marks);
-    window.averageMark.textContent = averageMark;
+  let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
+  let averageMark = getAverageMark(marks);
+  window.averageMark.textContent = averageMark;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
-
-    let average = marks;
+  // код для задачи №2 писать здесь
     
-    if (average.length > 5) {
-      window.alert(`Внимание! Превышено количество введённых данных. Будут учтены только первые пять оценок.`);
-      average = average.slice(0, 5);
-    }
+  let average = marks;
     
-    let averageMark = average.reduce((accumulator, average) => accumulator + average, 0)  / average.length;
+  if (average.length > 5) {
+    console.log('Внимание! Превышено количество введённых данных. Будут учтены только первые пять оценок.');
+    average = average.slice(0, 5);
+  }
+    
+  let averageMark = average.reduce((accumulator, average) => accumulator + average, 0)  / average.length;
 
-    // console.log('кол-во оценок ' + average.length);
-    // console.log('массив ' + average);
-    // console.log('среднее ' + averageMark);
-
-    return averageMark;
+  return averageMark;
 }
 
 getAverageMark();
 
-//--------------------------------------------------------
 function calculateDrinkTask(){
-    let name = window.personName.value;
-    let dateOfBirthday = new Date(window.dateOfBirthday.value);
-    let drink = askDrink(name, dateOfBirthday);
-    window.drink.textContent = drink;
+  let name = window.personName.value;
+  let dateOfBirthday = new Date(window.dateOfBirthday.value);
+  let drink = askDrink(name, dateOfBirthday);
+  window.drink.textContent = drink;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
-  
-    let today = new Date();
-    // console.log(today);
-    
-    let age = today.getFullYear() - dateOfBirthday.getFullYear();
-    // console.log('full: ' + age);
+  // код для задачи №3 писать здесь
+  //console.log(result); 
 
-    var result =  (age >= 18) ? (`Не желаете ли олд-фэшн, ${name}?`) : (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
-    console.log(result);
-    return result;
+  let today = new Date();
+       
+  let age = today.getFullYear() - dateOfBirthday.getFullYear();
+    
+  let result = (age >= 18) ? (`Не желаете ли олд-фэшн, ${name}?`) : (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    
+  return result;
 }
 
 askDrink();
-    
